0140e60dc769519ebf6526b0b42e65a59753d4cd
TechdElas <90809864+BeaSouzaa@users.noreply.github.com>
2021-12-06 23:51:19 -0300

    Consumo de API - Model e Service

--- blogPessoal/src/app/app.module.ts
+++ blogPessoal/src/app/app.module.ts
@@ -1,13 +1,18 @@
+//Módulos
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http'
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
+
+
+//Componentes
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { RodapeComponent } from './rodape/rodape.component';
 import { EntrarComponent } from './entrar/entrar.component';
 import { CadastrarComponent } from './cadastrar/cadastrar.component';
-import { ModelComponent } from './model/model.component';
+
 
 @NgModule({
   declarations: [
@@ -16,11 +21,13 @@ import { ModelComponent } from './model/model.component';
     RodapeComponent,
     EntrarComponent,
     CadastrarComponent,
-    ModelComponent
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule, 
+    HttpClientModule, 
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
