b7d8c557ad4108f6958e62525363e95f6277d0b2
TechdElas <90809864+BeaSouzaa@users.noreply.github.com>
2021-12-10 09:46:10 -0300

    Componente inicio sem consumo

--- blogPessoal/src/app/app.module.ts
+++ blogPessoal/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http'
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
-
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 //Componentes
 import { AppComponent } from './app.component';
@@ -31,7 +31,10 @@ import { InicioComponent } from './inicio/inicio.component';
     HttpClientModule, 
     FormsModule
   ],
-  providers: [],
+  providers: [
+    {provide: LocationStrategy,
+    useClass: HashLocationStrategy}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
